8057319a32b0a142bff57bc2549ffcb057ac2587
buter_brodskij <[vderzhanovich@gmail.com]>
2022-08-22 13:59:38 +0300

    add file

---
+++ itfinder/itfinder/views.py
@@ -0,0 +1,7 @@
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('projects')
--- itfinder/projects/admin.py
+++ itfinder/projects/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Project, Review, Tag
-# buter
-# Tenusi34!
 
 admin.site.register(Project)
 admin.site.register(Review)
