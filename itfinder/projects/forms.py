6fb6555665dfe90c4c241f088bd3821a263258b9
Uladzislau Dzerzhanovich <vderzhanovich2@gmail.com>
2022-08-27 14:42:55 +0300

    add file

--- itfinder/projects/forms.py
+++ itfinder/projects/forms.py
@@ -1,13 +1,12 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Project
+from .models import Project, Review
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'slug', 'image', 'tags', 'description', 'demo_link', 'source_link']
-        exclude = ['objects', 'total_votes', 'votes_ratio', 'created', 'id']
         labels = {
             'title': 'Название проекта',
             'slug': 'Слаг',
@@ -22,7 +21,25 @@ class ProjectForm(ModelForm):
             'tags': forms.CheckboxSelectMultiple(),
         }
 
-        def __init__(self, *args, **kwargs):
-            super(ProjectForm, self).__init__(*args, **kwargs)
-            for name, field in self.fields.items():
-                field.widget.attrs.update({'class': 'input'})
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+
+        labels = {
+            'value': 'Поставьте оценку проекту',
+            'body': 'Добавьте отзыв о проекте'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
